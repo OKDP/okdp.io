cbb283c9539ee3b7a465fc56c80ff455c0104309
Moncef LAHOUAR <moncef.lahouar@kubotal.io>
2026-04-03 14:37:55 +0200

    feat: revamp architecture section and add dedicated roadmap page

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@ const { resolve } = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
+    base: './',
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
                 en: resolve(__dirname, 'en/index.html'),
+                roadmap: resolve(__dirname, 'roadmap/index.html'),
+                enRoadmap: resolve(__dirname, 'en/roadmap/index.html'),
             },
         },
     },
